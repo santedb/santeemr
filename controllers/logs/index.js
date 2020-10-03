4649f6037ef594c93fcf811ee80ea7285803ac31
Justin Fyfe <justin@fyfesoftware.ca>
2020-10-02 20:25:23 -0400

    Updated the next of kin edit and fixed versioning issues

---
+++ controllers/logs/index.js
@@ -0,0 +1,117 @@
+/// <reference path="../../.ref/js/santedb.js"/>
+/*
+ * Portions Copyright 2015-2019 Mohawk College of Applied Arts and Technology
+ * Portions Copyright 2019-2019 SanteSuite Contributors (See NOTICE)
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you 
+ * may not use this file except in compliance with the License. You may 
+ * obtain a copy of the License at 
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0 
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the 
+ * License for the specific language governing permissions and limitations under 
+ * the License.
+ * 
+ * User: Justin Fyfe
+ * Date: 2019-10-3
+ */
+angular.module('santedb').controller('LogIndexController', ["$scope", "$rootScope", "$timeout", "$compile", function ($scope, $rootScope, $timeout, $compile) {
+ 
+    
+    // Change handler for show option
+    $scope.updateView = function() {
+        dt.ajax.reload();
+    }
+
+    // Initial value 
+    $scope.extern = 'false';
+    var dt = $("#logInfoTable").DataTable({
+        serverSide: true,
+        buttons: [
+            'reload'
+        ],
+        columns: [
+            {
+                data: 'name',
+                orderable: true
+            },
+            {
+                orderable: true,
+                class: "d-none d-md-table-cell",
+                render: function(d, t, r) {
+                    return moment(r.modified).format(SanteDB.locale.dateFormats.full);
+                }
+            },
+            {
+                orderable: true,
+                class: "d-none d-sm-table-cell",
+                render: function(d, t, r) {
+                    return (r.size / 1024).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,') + " KB";
+                }
+            },
+            {
+                orderable: false,
+                render: function(d, t, r, i) {
+                    return `<a class="btn btn-primary" ui-sref="santedb-emr.system.logs.view({ id: '${r.name}' })"><i class="fas fa-eye"></i> <span
+                    class="d-none d-md-inline"> {{ 'ui.action.view' | i18n }}</span></a>`;
+                }
+            }
+        ],
+        ajax: function (data, callback, settings) {
+
+            var query = {
+                _upstream: $scope.extern,
+                _count: data.length,
+                _offset: data.start
+            };
+            if (data.search.value.length > 0)
+                query.name = `~${data.search.value}`;
+            if (data.order.length > 0) {
+                
+                var colname = null;
+                switch(data.order[0].column)
+                {
+                    case 0:
+                        colname = "name";
+                        break;
+                    case 1:
+                        colname = "modified";
+                        break;
+                    case 2: 
+                        colname = "size";
+                        break;
+                }
+                query["_orderBy"] = `${colname}:${data.order[0].dir}`;
+            }
+
+            SanteDB.application.getLogInfoAsync(null, query)
+                .then(function (res) {
+                    callback({
+                        data: res.resource.map(function (item) {
+                            return item;
+                        }),
+                        recordsTotal: undefined,
+                        recordsFiltered: res.totalResults || res.size
+                    });
+                })
+                .catch(function (err) { $rootScope.errorHandler(err) });
+        },
+        createdRow: function (r, d, i) {
+            $compile(angular.element(r).contents())($scope);
+            $scope.$digest()
+        },
+    });
+
+    // Bind buttons
+    var bindButtons = function () {
+        dt.buttons().container().appendTo($('.col-md-6:eq(0)', dt.table().container()));
+        if (dt.buttons().container().length == 0)
+            $timeout(bindButtons, 100);
+        
+    };
+    bindButtons();
+ }]);
+ 
---
+++ controllers/system/index.js
@@ -0,0 +1,76 @@
+/// <reference path="../../.ref/js/santedb.js"/>
+/*
+ * Portions Copyright 2015-2019 Mohawk College of Applied Arts and Technology
+ * Portions Copyright 2019-2019 SanteSuite Contributors (See NOTICE)
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you 
+ * may not use this file except in compliance with the License. You may 
+ * obtain a copy of the License at 
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0 
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the 
+ * License for the specific language governing permissions and limitations under 
+ * the License.
+ * 
+ * User: Justin Fyfe
+ * Date: 2019-10-5
+ */
+angular.module('santedb').controller('SystemInfoController', ["$scope", "$rootScope", "$state", "$interval", function ($scope, $rootScope, $state, $interval) {
+
+
+    $scope.isLocalLoading = true;
+
+    // Get application information
+    SanteDB.application.getAppInfoAsync({ updates: false })
+        .then(function(d) {
+            $scope.info = d;
+            $scope.isLocalLoading = false;
+            $scope.$apply();
+        })
+        .catch($rootScope.errorHandler);
+
+   
+    $scope.enableService = async function(serviceId) {
+        if(confirm(SanteDB.locale.getString("ui.emr.system.confirm.enableService"))) {
+            try {
+                serviceId = serviceId.substr(0, serviceId.indexOf(','));
+                await SanteDB.api.app.postAsync({
+                    resource: `Configuration/Service/${serviceId}`,
+                    data: {},
+                    contentType: 'application/json'
+                });
+                $scope.info = await SanteDB.application.getAppInfoAsync({ updates: false });
+                alert(SanteDB.locale.getString('ui.emr.system.confirm.serviceChange'));
+                try {
+                    $scope.$apply();
+                } catch(e) {}
+            }
+            catch(e) {
+                $rootScope.errorHandler(e);
+            }
+        }
+    }
+
+    $scope.disableService = async function(serviceId) {
+        if(confirm(SanteDB.locale.getString("ui.emr.system.confirm.disableService"))) {
+            try {
+                serviceId = serviceId.substr(0, serviceId.indexOf(','));
+                await SanteDB.api.app.deleteAsync({
+                    resource: `Configuration/Service/${serviceId}`,
+                });
+
+                $scope.info = await SanteDB.application.getAppInfoAsync({ updates: false });
+                alert(SanteDB.locale.getString('ui.emr.system.confirm.serviceChange'));
+                try {
+                    $scope.$apply();
+                } catch(e) {}
+            }
+            catch(e) {
+                $rootScope.errorHandler(e);
+            }
+        }
+    }
+}]);
