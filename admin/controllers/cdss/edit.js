f26c1089aac60ba196fa12a85ad59c6db798dad4
Justin Fyfe <justin@fyfesoftware.ca>
2023-11-24 15:37:13 -0500

    Added history - added rendering of logic blocks

--- admin/controllers/cdss/edit.js
+++ admin/controllers/cdss/edit.js
@@ -1,6 +1,6 @@
 /// <reference path="../../.ref/js/santedb.js"/>
 angular.module('santedb').controller('CdssEditController', ["$scope", "$rootScope", "$timeout", "$state", "$stateParams", function ($scope, $rootScope, $timeout, $state, $stateParams) {
-    
+
     async function checkDuplicate(query) {
         try {
             query._includeTotal = true;
@@ -17,8 +17,14 @@ angular.module('santedb').controller('CdssEditController', ["$scope", "$rootScop
 
     async function initializeView(id) {
         try {
-            var cdssLibrary = await SanteDB.resources.cdssLibraryDefinition.getAsync(id, 'full', null, true);
-            $timeout(() => $scope.cdssLibrary = cdssLibrary);
+            var libraryDefinition = await SanteDB.resources.cdssLibraryDefinition.getAsync(id, null, null, true);
+            $timeout(() => {
+                $scope.cdssLibrary = libraryDefinition;
+            });
+            var libraryHistory = await SanteDB.resources.cdssLibraryDefinition.findAssociatedAsync(id, "_history", null, null, true);
+            $timeout(() =>{
+                $scope.cdssLibrary._history = libraryHistory.resource;
+            });
         }
         catch (e) {
             $rootScope.errorHandler(e);
@@ -40,20 +46,20 @@ angular.module('santedb').controller('CdssEditController', ["$scope", "$rootScop
     async function saveCdssLibrary(form) {
         // Mark duplicates
         var failedValidation = false;
-        if($scope.cdssLibrary.library.oid) {
-            var valid = await checkDuplicate({oid: $scope.cdssLibrary.library.oid});
+        if ($scope.cdssLibrary.library.oid) {
+            var valid = await checkDuplicate({ oid: $scope.cdssLibrary.library.oid });
             $timeout(() => $scope.saveCdssLibraryForm.libraryOid.$setValidity('duplicate', valid));
-            failedValidation = valid;
+            failedValidation |= !valid;
         }
-        if($scope.cdssLibrary.library.id) {
-            var valid = await checkDuplicate({id: $scope.cdssLibrary.library.id});
+        if ($scope.cdssLibrary.library.id) {
+            var valid = await checkDuplicate({ id: $scope.cdssLibrary.library.id });
             $timeout(() => $scope.saveCdssLibraryForm.libraryId.$setValidity('duplicate', valid));
-            failedValidation = valid;
+            failedValidation |= !valid;
         }
-        if($scope.cdssLibrary.library.name) {
-            var valid = await checkDuplicate({name: $scope.cdssLibrary.library.name});
+        if ($scope.cdssLibrary.library.name) {
+            var valid = await checkDuplicate({ name: $scope.cdssLibrary.library.name });
             $timeout(() => $scope.saveCdssLibraryForm.libraryName.$setValidity('duplicate', valid));
-            failedValidation = valid;
+            failedValidation |= !valid;
         }
 
         if (form.$invalid || failedValidation) {
@@ -71,7 +77,7 @@ angular.module('santedb').controller('CdssEditController', ["$scope", "$rootScop
                     SanteDB.display.buttonWait("#btnSaveLibrary", true);
 
                     var library = null;
-                    if($scope.cdssLibrary.id) {
+                    if ($scope.cdssLibrary.id) {
                         library = await SanteDB.resources.cdssLibraryDefinition.updateAsync($scope.cdssLibrary.id, $scope.cdssLibrary, true);
                     }
                     else {
@@ -91,7 +97,7 @@ angular.module('santedb').controller('CdssEditController', ["$scope", "$rootScop
         }
     }
 
-    function uploadCdssLibraryDefinition(form) {
+    function uploadCdssLibraryDefinition(form, originalId) {
         var file_data = form.source.$$element.prop('files')[0];
         var form_data = new FormData();
         form_data.append('source', file_data);
@@ -103,15 +109,20 @@ angular.module('santedb').controller('CdssEditController', ["$scope", "$rootScop
             headers: {
                 "X-SanteDB-Upstream": true
             },
-            method: 'POST',
+            method: originalId ? 'PUT' : 'POST',
             dataType: 'json',
-            url: "/ami/CdssLibraryDefinition",
+            url: originalId ? `/ami/CdssLibraryDefinition/${originalId}` : "/ami/CdssLibraryDefinition",
             data: form_data,
             success: function (data) {
                 try {
                     if (!data.issue || data.issue.length == 0) {
                         toastr.success(SanteDB.locale.getString('ui.admin.cdss.upload.success'));
-                        $state.go('santedb-admin.emr.cdss.view', { id: data.id });
+                        if (!originalId) {
+                            $state.go('santedb-admin.emr.cdss.view', { id: data.id });
+                        }
+                        else {
+                            $state.reload();
+                        }
                     }
                     else {
                         toastr.success(SanteDB.locale.getString('ui.admin.cdss.upload.error', { error: data.message }));
@@ -134,6 +145,22 @@ angular.module('santedb').controller('CdssEditController', ["$scope", "$rootScop
         });
     }
 
+    $scope.downloadCdssLibrary = function (id) {
+        var win = window.open(`/ami/CdssLibraryDefinition/${$scope.cdssLibrary.id}?_format=xml&_upstream=true`, '_blank');
+        win.onload = function (e) {
+            win.close();
+        };
+    }
+
+    $scope.uploadReplacementLibrary = function () {
+        $("#uploadCdssLibraryModal").modal('show');
+    }
 
     $scope.saveCdssLibrary = saveCdssLibrary;
+    $scope.uploadCdssLibraryDefinition = function (form) {
+        if(form.$invalid) {
+            return;
+        }
+        uploadCdssLibraryDefinition(form, $scope.cdssLibrary.id);
+    }
 }]);
