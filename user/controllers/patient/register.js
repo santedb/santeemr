ef1c1c7ad07205e9d203da74479f54b7e44f6599
Justin Fyfe <justin@fyfesoftware.ca>
2024-06-03 17:32:23 -0400

    Added mother's information panel. Updated the identifier panel. Updated the boostrap theme to look better on the EMR

--- user/controllers/patient/register.js
+++ user/controllers/patient/register.js
@@ -48,8 +48,49 @@ angular.module('santedb').controller('EmrPatientRegisterController', ["$scope", 
     $scope.$on("$destroy", function(s) {
         window.onbeforeunload = null;
     })
-}]).controller("EmrPatientRegisterWidgetController", ["$scope", "$rootScope", "$timeout", function($scope, $rootScope, $timeout) {
+}])
+// CONTROLLER -> Generic functions for registration widgets (note: should not $watch or initialize data)
+.controller("EmrPatientRegisterWidgetController", ["$scope", "$rootScope", "$timeout", function($scope, $rootScope, $timeout) {
 
     $scope.ageToDate = ageToDate;
     $scope.dateToAge = dateToAge;
-}]);
+
+    async function lookupRelative(relativeType, identifierList) {
+        try {
+            
+            var filter = {
+                _includeTotal: true,
+                _count: 1
+            };
+
+            Object.keys(identifierList).forEach(o=>{
+                var identifiers = identifierList[o].filter(id=>id.value && id.value != "");
+                if(identifiers.length > 0) {
+                    filter[`identifier[${o}].value`] = identifiers.map(o=>o.value);
+                }
+            });
+
+            if(Object.keys(filter).length > 2) {
+                var matches = await SanteDB.resources.person.findAsync(filter, "min");
+                if(matches.totalResults == 1) {
+                    $scope.scopedObject.relationship[relativeType][0] = new Person(matches.resource[0]); // Copy the information from the other relative
+                }
+                else if(matches.totalResults > 1) {
+                    toastr.warn(SanteDB.locale.getString("ui.emr.patient.register.relative.multipleMatches", { relativeType: relativeType }));
+                }
+            }
+        }
+        catch(e) {
+            toastr.warn(SanteDB.locale.getString("ui.emr.patient.register.relative.errorSearching", { error: e.message }));
+        }
+    }
+
+    if($scope.panel.name == "org.santedb.emr.widget.patient.register.mother") {
+        $scope.$watch(scope => JSON.stringify(scope.scopedObject.relationship.Mother[0].identifier), function(n, o) {
+            if(n && n != o) {
+                lookupRelative("Mother", $scope.scopedObject.relationship.Mother[0].identifier);
+            }
+        });
+    }
+
+}])
