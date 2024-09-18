13ad2cb4ef11e9633eb0df6e177c64925eea9acb
Justin Fyfe <justin@fyfesoftware.ca>
2024-09-18 15:43:00 -0400

    Completed the editing of care pathway information

--- admin/controllers/carePaths/edit.js
+++ admin/controllers/carePaths/edit.js
@@ -1,13 +1,14 @@
-angular.module("santedb").controller("EmrAdminCarePathEditController", ["$scope", "$rootScope", "$timeout", "$stateParams", function($scope, $rootScope, $timeout, $stateParams) {
+angular.module("santedb").controller("EmrAdminCarePathEditController", ["$scope", "$rootScope", "$timeout", "$stateParams", "$state", function($scope, $rootScope, $timeout, $stateParams, $state) {
 
 
+    // Initialize the view
     async function initializeView(pathwayId) {
         try {
             var pathway = null;
             if(pathwayId) {
                 pathway = await SanteDB.resources.carePathwayDefinition.getAsync(pathwayId);
                 pathway.enrollment = `${pathway.enrollment}`;
-                pathway.eligibility = pathway.eligibility.split("&");
+                pathway.eligibility = pathway.eligibility.split("&").map(o => { return { expr: o }});
             }
             else {
                 pathway = new CarePathwayDefinition({
@@ -23,5 +24,28 @@ angular.module("santedb").controller("EmrAdminCarePathEditController", ["$scope"
         }
     }
 
+    async function saveCarePath(carePathForm) {
+        if(carePathForm.$invalid) return;
+
+        try {
+
+            // Collapse the eligibility criteria
+            $scope.pathway.eligibility = $scope.pathway.eligibility.map(o=>o.expr).join("&");
+            if($scope.pathway.id) {
+                await SanteDB.resources.carePathwayDefinition.updateAsync($scope.pathway.id, $scope.pathway);
+            }
+            else {
+                await SanteDB.resources.carePathwayDefinition.insertAsync($scope.pathway);
+            }
+            toastr.success(SanteDB.locale.getString("ui.emr.admin.carePath.save.success"));
+            $state.go("santedb-admin.emr.carePaths.index");
+        }
+        catch(e) {
+            $rootScope.errorHandler(e);
+        }
+    }
+
     initializeView($stateParams.id);
+
+    $scope.saveCarePath = saveCarePath;
 }]);
--- admin/controllers/carePaths/index.js
+++ admin/controllers/carePaths/index.js
@@ -2,5 +2,19 @@ angular.module("santedb").controller("EmrAdminCarePathController", ["$scope", "$
 
     $scope.renderEnrollment = (r) => r.enrollment == 0 ? "Manual" : "Auto";
 
-    $scope.renderEligibility = (r) => `<code>${r.eligibility.replaceAll("<", "&lt;")}</code>`;
+    $scope.renderEligibility = (r) => `<code>${r.eligibility.replaceAll("<", "lt;").split("&").join("<br/>").replaceAll("lt;", "&lt;")}</code>`;
+
+    $scope.deleteCarePath = async function(id) {
+        if(confirm(SanteDB.locale.getString("ui.emr.admin.carePath.delete.confirm"))) {
+            try {
+                await SanteDB.resources.carePathwayDefinition.deleteAsync(id);
+                toastr.success(SanteDB.locale.getString("ui.emr.admin.carePath.delete.success"));
+                $("#carePathTable").attr("newQueryId", true);
+                $("#carePathTable table").DataTable().draw();
+            }
+            catch (e) {
+                $rootScope.errorHandler(e);
+            }
+        }
+    }
 }]);
