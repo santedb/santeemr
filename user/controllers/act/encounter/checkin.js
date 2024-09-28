f9635f801ce7cf46d41faf71e7344caa62181225
Justin Fyfe <justin@fyfesoftware.ca>
2024-09-28 15:52:59 -0400

    Updated the patient monitoring

--- user/controllers/act/encounter/checkin.js
+++ user/controllers/act/encounter/checkin.js
@@ -1,29 +1,31 @@
 /// <reference path="../../../.ref/js/santedb.js" />
 angular.module('santedb').controller('EmrCheckinEncounterController', ["$scope", "$rootScope", "$timeout", function ($scope, $rootScope, $timeout) {
-    
+
     $scope.patientId = null;
-    $scope.$watch("patientId", async function(n, o) {
-        if(n && n != o) {
+    $scope.$watch("patientId", async function (n, o) {
+        if (n && n != o) {
             try {
                 var patient = await SanteDB.resources.patient.getAsync(n, "full");
                 $timeout(() => $scope.recordTarget = new Patient(patient));
             }
-            catch(e) {
+            catch (e) {
                 $rootScope.errorHandler(e);
             }
         }
     });
 
-    $scope.applyGuardExpression = function(data) {
-        if(data.guard) {
+    $scope.applyGuardExpression = function (data) {
+        if (data.guard) {
             return $scope.$eval(data.guard, { recordTarget: $scope.recordTarget });
         }
         return true;
     }
 
-    $("#checkinModal").on("hidden.bs.modal", function() {
-        delete($scope.patientId);
-        delete($scope.recordTarget);
-        delete($scope.encounter);
+    $("#checkinModal").on("hidden.bs.modal", function () {
+        $timeout(() => {
+            $scope.patientId = "";
+            delete ($scope.recordTarget);
+            delete ($scope.encounter);
+        });
     });
 }]);
