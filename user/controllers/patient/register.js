3cbfbcc1ec12df0b7b90a436fab75f21d3bf5af6
Justin Fyfe <justin@fyfesoftware.ca>
2025-02-19 17:40:25 -0500

    Updated __AddressPlaceRef to PlaceRef

--- user/controllers/patient/register.js
+++ user/controllers/patient/register.js
@@ -67,10 +67,10 @@ angular.module('santedb').controller('EmrPatientRegisterController', ["$scope", 
         if (n && n != o) {
             try {
                 if ($scope.entity.address.HomeAddress[0].component &&
-                    $scope.entity.address.HomeAddress[0].component._AddressPlaceRef &&
-                    $scope.entity.address.HomeAddress[0].component._AddressPlaceRef[0]
+                    $scope.entity.address.HomeAddress[0].component.PlaceRef &&
+                    $scope.entity.address.HomeAddress[0].component.PlaceRef[0]
                 ) {
-                    var dsdl = await SanteDB.resources.place.findAsync({ "classConcept": EntityClassKeys.ServiceDeliveryLocation, "relationship[CommunityServiceDeliveryLocation].source||relationship[CommunityServiceDeliveryLocation].source.relationship[Parent].source": $scope.entity.address.HomeAddress[0].component._AddressPlaceRef, _count: 1, _includeTotal: true });
+                    var dsdl = await SanteDB.resources.place.findAsync({ "classConcept": EntityClassKeys.ServiceDeliveryLocation, "relationship[CommunityServiceDeliveryLocation].source||relationship[CommunityServiceDeliveryLocation].source.relationship[Parent].source": $scope.entity.address.HomeAddress[0].component.PlaceRef, _count: 1, _includeTotal: true });
                     if (dsdl.totalResults == 1) // one result!
                     {
                         $timeout(() => {
