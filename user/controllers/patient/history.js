33721ea824ebbd8956cad8913b214db6b0ec7ab6
Nutley <rjnutley@gmail.com>
2025-06-04 14:23:59 -0400

    height/weight history

--- user/controllers/patient/history.js
+++ user/controllers/patient/history.js
@@ -3,7 +3,22 @@ angular.module('santedb').controller('EmrPatientHistoryViewController', ["$scope
     async function initialize() {     
         const heightWeightBackEntry = {};
 
-        for (const act of $scope.$parent.acts) {
+        const userEntityId = await SanteDB.authentication.getCurrentUserEntityId();
+        const facilityId = await SanteDB.authentication.getCurrentFacilityId();        
+
+        for (const act of $scope.$parent.acts) {            
+          // Add facility to participant
+          act.participation = act.participation || {};
+          act.participation.Location = act.participation.Location || [];
+          act.participation.Location.push(new ActParticipation({ player: facilityId }));
+          
+          act.participation.Authororiginator = act.participation.Authororiginator || [];
+          act.participation.Authororiginator.push(new ActParticipation({ player: userEntityId }));
+
+          act.tag = act.tag || {};
+          act.tag.isBackEntry = ["true"];
+          act.operation = BatchOperationType.InsertOrUpdate;
+
           const year = act.actTime.getFullYear();       
           const month = act.actTime.getMonth() + 1;    
 
