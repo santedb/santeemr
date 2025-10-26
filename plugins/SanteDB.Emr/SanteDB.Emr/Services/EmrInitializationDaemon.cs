5b2f4cc510d9dcf7dc92785c9cf9030cbd7b11ff
Justin Fyfe <justin@fyfesoftware.ca>
2025-10-26 14:07:32 -0400

    Updated to remove old reference

--- plugins/SanteDB.Emr/SanteDB.Emr/Services/EmrInitializationDaemon.cs
+++ plugins/SanteDB.Emr/SanteDB.Emr/Services/EmrInitializationDaemon.cs
@@ -58,7 +58,6 @@ namespace SanteEMR.Services
             if(this.m_shouldBindServices)
             {
                 this.m_tracer.TraceInfo("Binding EMR Service Events");
-                this.m_serviceProvider.AddBusinessRule<Bundle>(typeof(PregnancyResolutionRule));
                 this.m_serviceProvider.AddBusinessRule<Bundle>(typeof(BundleStatusObservationRule));
             }
             
