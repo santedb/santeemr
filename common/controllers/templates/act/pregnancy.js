871aaf76407fe4d7eb86a34cf7e8084aca83f931
Justin Fyfe <justin@fyfesoftware.ca>
2026-01-06 17:20:23 -0500

    JIMS-1056 #done

--- common/controllers/templates/act/pregnancy.js
+++ common/controllers/templates/act/pregnancy.js
@@ -26,7 +26,7 @@ angular.module('santedb').controller("PregnancyHistoryController", ["$scope", "$
             }
 
             // Populate the episode link if needed
-            if (act.relationship?.RefersTo && 
+            if (act.relationship?.RefersTo &&
                 !$scope.$eval("act.relationship?.RefersTo[0].target")
             ) {
                 SanteDB.resources.act.findAsync({
@@ -44,15 +44,15 @@ angular.module('santedb').controller("PregnancyHistoryController", ["$scope", "$
                 });
             }
             // Determine if the current act has a delivery outcome and use that as our delivery outcome
-            if(act.typeConcept == '983152a6-be28-4fc0-9c60-63e7178060f7' && $stateParams.id && !act.value) {
+            if (act.typeConcept == '983152a6-be28-4fc0-9c60-63e7178060f7' && $stateParams.id && !act.value) {
                 SanteDB.resources.codedObservation.findAsync({
-                    "typeConcept" : "dddf18e4-1868-11eb-adc1-0242ac120002",
-                    "relationship[HasComponent].source.relationship[HasComponent].source.relationship[HasComponent].source" : $stateParams.id,
-                    "statusConcept" : StatusKeys.Completed,
-                    "_count" : 1, 
-                    "_includeTotal" : false
+                    "typeConcept": "dddf18e4-1868-11eb-adc1-0242ac120002",
+                    "relationship[HasComponent].source.relationship[HasComponent].source.relationship[HasComponent].source": $stateParams.id,
+                    "statusConcept": StatusKeys.Completed,
+                    "_count": 1,
+                    "_includeTotal": false
                 }, "min").then((obs) => {
-                    $timeout(()=> act.value = obs.value)
+                    $timeout(() => act.value = obs.value)
                 });
             }
 
@@ -73,4 +73,75 @@ angular.module('santedb').controller("PregnancyHistoryController", ["$scope", "$
     }
 
     initializeView($scope.act);
+}]).controller("PregnancyStatusController", ["$scope", "$rootScope", "$timeout", function ($scope, $rootScope, $timeout) {
+    var _pregnancyNegativeConcepts = [];
+    async function initializeView() {
+        try {
+            var conceptSetExpansion = await SanteDB.resources.conceptSet.findAssociatedAsync('8e848f0e-890b-11f0-ae3f-9f37087e0822', "Concept", null, "min");
+            _pregnancyNegativeConcepts = conceptSetExpansion.resource.map(o => o.id);
+
+            // Is the patient currently pregnant? If so - we will be updating the pregnancy status 
+            if ($scope.act.tag?.$userAdded) {
+                var currentPregnancy = await SanteDB.resources.codedObservation.findAsync({
+                    "relationship[RefersTo].target.typeConcept": "7bb3403e-d8ee-4b91-8a77-64da4f459415",
+                    "participation[RecordTarget].player": $scope.act.participation.RecordTarget[0].player,
+                    "typeConcept": "236b5641-61d2-4d12-91f7-5dddbd7f8931",
+                    "statusConcept": StatusKeys.Active,
+                    "_count": 1,
+                    "_includeTotal": false
+                }, "emr.actSummaryView");
+
+                if (currentPregnancy.resource) {
+                    currentPregnancy = currentPregnancy.resource[0].relationship.RefersTo[0].targetModel;
+                    console.info(currentPregnancy);
+                    $timeout(() => {
+                        $scope.act.value = currentPregnancy.value;
+                        $scope.act.id = currentPregnancy.id;
+                        $scope.act.operation = BatchOperationType.UpdateInt;
+
+                        // Populate the sub-components to allow for update
+                        $scope.act.relationship.HasComponent.forEach(comp => {
+                            // Find the current pregnancy component of the same type
+                            var currentComponent = currentPregnancy.relationship.HasComponent?.find(current => current.targetModel?.typeConcept == comp.targetModel.typeConcept);
+                            if (currentComponent) {
+                                comp.target = currentComponent.target;
+                                comp.targetModel = currentComponent.targetModel;
+                                comp.targetModel.operation = BatchOperationType.UpdateInt;
+                            }
+                        });
+                    });
+                }
+            }
+        }
+        catch (e) {
+            console.error(e);
+        }
+    }
+
+    initializeView();
+
+    function showOrHideComponents(selectedValue) {
+        if (!selectedValue || selectedValue == EmptyGuid || _pregnancyNegativeConcepts.includes(selectedValue)) {
+            $scope.act.relationship?.HasComponent?.forEach(n => n.operation = BatchOperationType.Ignore);
+        }
+        else {
+            $scope.act.relationship?.HasComponent?.forEach(n => n.operation = BatchOperationType.InsertOrUpdate);
+        }
+    }
+
+    $scope.resolveTemplate = function (templateId) {
+        var templateValue = SanteDB.application.resolveTemplateForm(templateId);
+        if (templateValue == null) {
+            return "/org.santedb.uicore/partials/act/noTemplate.html"
+        }
+        return templateValue;
+    }
+
+
+    $scope.$watch("act.value", function (n, o) {
+        showOrHideComponents(n);
+    });
+
+    showOrHideComponents($scope.act.value);
+
 }]);
